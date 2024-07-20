d639d45c644e8e728985681eb2ae5526a4242ad0
Jxkeorton <jakeorton99@gmail.com>
2024-07-20 14:11:42 +0100

    Add 'tandems' and 'courses' apps for the non user interactions

---
+++ tandems/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TandemsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'tandems'
---
+++ tandems/views.py
@@ -0,0 +1,4 @@
+from django.http import HttpResponse
+
+def tandems(request):
+    return HttpResponse("Tandems")
