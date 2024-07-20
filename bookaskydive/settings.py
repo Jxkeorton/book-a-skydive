b358b230538052cec9fa3f3f697d6a07b577f501
Jxkeorton <jakeorton99@gmail.com>
2024-07-20 17:00:39 +0100

    configure for deployment

--- bookaskydive/settings.py
+++ bookaskydive/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7rf2@8@9&@4rw4^jpz)g#1v6f8@c1nk*z1lw05a*!_5$z_aob$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
