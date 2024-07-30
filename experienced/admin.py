5556c29c1d218f6cc6ee1a788f14bfe8494bfcd2
Jxkeorton <jakeorton99@gmail.com>
2024-07-30 20:24:57 +0100

    Add experienced models to database

--- experienced/admin.py
+++ experienced/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Plane, JumpSlot, JumpBooking
 
-# Register your models here.
+class PlaneAdmin(admin.ModelAdmin):
+    list_display = ('name', 'capacity')
+    search_fields = ('name',)
+    ordering = ('name',)
+
+class JumpSlotAdmin(admin.ModelAdmin):
+    list_display = ('plane', 'jump_date', 'available_slots')
+    list_filter = ('plane', 'jump_date')
+    search_fields = ('plane__name',)
+    ordering = ('-jump_date',)
+
+class JumpBookingAdmin(admin.ModelAdmin):
+    list_display = ('user', 'jump_slot', 'booking_date')
+    list_filter = ('jump_slot__plane', 'jump_slot__jump_date', 'booking_date')
+    search_fields = ('user__username', 'jump_slot__plane__name')
+    ordering = ('-booking_date',)
+
+admin.site.register(Plane, PlaneAdmin)
+admin.site.register(JumpSlot, JumpSlotAdmin)
+admin.site.register(JumpBooking, JumpBookingAdmin)
--- experienced/models.py
+++ experienced/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Plane(models.Model):
+    name = models.CharField(max_length=200)
+    capacity = models.IntegerField()  # Maximum number of jumpers
+
+    def __str__(self):
+        return self.name
+
+class JumpSlot(models.Model):
+    plane = models.ForeignKey(Plane, on_delete=models.CASCADE)
+    jump_date = models.DateField()
+    available_slots = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.plane.name} on {self.jump_date}"
+
+class JumpBooking(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    jump_slot = models.ForeignKey(JumpSlot, on_delete=models.CASCADE)
+    booking_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} booked {self.jump_slot.plane.name} on {self.jump_slot.jump_date}"
