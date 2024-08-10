ae4321d2f2cce276ca924952d3ff89170e8eb486
Jxkeorton <jakeorton99@gmail.com>
2024-08-10 15:58:31 +0100

    Configure AllAuth

--- bookaskydive/urls.py
+++ bookaskydive/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('course/', include('courses.urls'), name='courses_urls'),
     path('user/', include('experienced.urls'), name="experienced_urls"),
     path('admin/', admin.site.urls),
+    path("accounts/", include("allauth.urls")),
 ]
