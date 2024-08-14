45f735daf08689ef420bc33164fd9fa51819f34d
Jxkeorton <jakeorton99@gmail.com>
2024-08-14 23:43:50 +0100

    Add logout styles and functionality

--- bookaskydive/settings.py
+++ bookaskydive/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1','.herokuapp.com']
 
@@ -142,7 +142,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
--- bookaskydive/urls.py
+++ bookaskydive/urls.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from courses import views as courses_views
-from tandems import views as tandems_views
 
 urlpatterns = [
     path('', include('tandems.urls'), name='tandems_urls'),
