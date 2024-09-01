22170cf268004b060af07896c9aa2e459ecf3961
Jxkeorton <jakeorton99@gmail.com>
2024-09-01 16:51:52 +0100

    Add tandem models

--- tandems/admin.py
+++ tandems/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import TandemDay, TandemTimeSlot
 
-# Register your models here.
+@admin.register(TandemDay)
+class TandemDayAdmin(admin.ModelAdmin):
+    list_display = ('date', 'max_tandems', 'total_booked', 'slots_available')
+    list_filter = ('date',)
+    search_fields = ('date',)
+    readonly_fields = ('total_booked', 'slots_available')
+
+@admin.register(TandemTimeSlot)
+class TandemTimeSlotAdmin(admin.ModelAdmin):
+    list_display = ('day', 'time', 'max_tandems', 'booked_tandems', 'slots_available')
+    list_filter = ('day', 'time',)
+    search_fields = ('day__date', 'time')
+    readonly_fields = ('slots_available',)
--- tandems/models.py
+++ tandems/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.utils import timezone
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+class TandemDay(models.Model):
+    date = models.DateField(unique=True)
+    max_tandems = models.PositiveIntegerField(default=50)
+
+    def __str__(self):
+        return self.date.strftime('%Y-%m-%d')
+
+    def clean(self):
+        if self.date < timezone.now().date():
+            raise ValidationError("Cannot create a TandemDay in the past.")
+
+    @property
+    def total_booked(self):
+        return sum(slot.booked_tandems for slot in self.timeslots.all())
+
+    @property
+    def slots_available(self):
+        return self.max_tandems > self.total_booked
+
+class TandemTimeSlot(models.Model):
+    day = models.ForeignKey(TandemDay, related_name='timeslots', on_delete=models.CASCADE)
+    time = models.TimeField()
+    max_tandems = models.PositiveIntegerField(default=6)
+    booked_tandems = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        unique_together = ('day', 'time')
+
+    def __str__(self):
+        return f'{self.day.date} at {self.time}'
+
+    @property
+    def slots_available(self):
+        return self.max_tandems > self.booked_tandems
