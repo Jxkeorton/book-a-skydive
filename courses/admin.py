3800bc93431bcf1493e6f8b6914b5b39eb5e9e93
Jxkeorton <jakeorton99@gmail.com>
2024-09-01 17:44:31 +0100

    Create and migrate courses app models

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import AFFCourse, VisitorDetail
 
-# Register your models here.
+@admin.register(AFFCourse)
+class AFFCourseAdmin(admin.ModelAdmin):
+    list_display = ('date', 'max_slots', 'booked_slots', 'slots_available')
+    list_filter = ('date',)
+    search_fields = ('date',)
+    readonly_fields = ('slots_available',)
+
+@admin.register(VisitorDetail)
+class VisitorDetailAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'email', 'phone_number', 'course')
+    list_filter = ('course',)
+    search_fields = ('full_name', 'email', 'course__date')
--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.utils import timezone
 
-# Create your models here.
+class AFFCourse(models.Model):
+    date = models.DateField(unique=True)
+    max_slots = models.PositiveIntegerField(default=6)
+    booked_slots = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f'AFF Course on {self.date}'
+
+    def clean(self):
+        if self.date < timezone.now().date():
+            raise ValidationError("Cannot schedule a course in the past.")
+        # Check if there's already a course scheduled for the same week
+        if AFFCourse.objects.filter(date__week=self.date.isocalendar()[1]).exclude(id=self.id).exists():
+            raise ValidationError("A course is already scheduled for this week.")
+
+    @property
+    def slots_available(self):
+        return self.max_slots > self.booked_slots
+
+class VisitorDetail(models.Model):
+    course = models.ForeignKey(AFFCourse, related_name='bookings', on_delete=models.CASCADE)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=15)
+    weight = models.PositiveIntegerField()
+    height = models.PositiveIntegerField()
+    full_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'{self.full_name} - {self.course.date}'
