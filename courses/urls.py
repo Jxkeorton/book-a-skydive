e2089f203a63d3b27e3b52be9efaca10d3d504db
Jxkeorton <jakeorton99@gmail.com>
2024-09-01 17:59:19 +0100

    fix urls.py

--- courses/urls.py
+++ courses/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.CourseListView.as_view(), name='list_courses'),  # List of available courses
-    path('book/<int:course_id>/', views.BookCourseView.as_view(), name='book_course'),  # Book a course
+    path('', views.list_courses, name='list_courses'),  # List available AFF courses
+    path('visitor_details/', views.visitor_details, name='visitor_details'),  # Page for entering visitor details
     path('booking_success/', views.booking_success, name='booking_success'),  # Booking success page
 ]
