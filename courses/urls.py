3e5c6b2e590c05c21c101c7c82683bac0a448095
Jxkeorton <jakeorton99@gmail.com>
2024-09-01 17:57:44 +0100

    Add the urls.py file to git

--- courses/urls.py
+++ courses/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.courses_list, name='course_list'),  # Example URL pattern
+    path('', views.CourseListView.as_view(), name='list_courses'),  # List of available courses
+    path('book/<int:course_id>/', views.BookCourseView.as_view(), name='book_course'),  # Book a course
+    path('booking_success/', views.booking_success, name='booking_success'),  # Booking success page
 ]
