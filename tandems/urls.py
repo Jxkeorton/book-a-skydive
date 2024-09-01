d9452973e22b9e9393f01fac60b4f4873830ff8c
Jxkeorton <jakeorton99@gmail.com>
2024-09-01 21:39:47 +0100

    Improve UI / UX for the tandems app

--- tandems/urls.py
+++ tandems/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('select-day/', views.select_day, name='select_day'),
     path('select-timeslot/<date>/', views.select_timeslot, name='select_timeslot'),
-    path('details/<int:timeslot_id>/', views.visitor_details, name='visitor_details'),
+    path('details/', views.visitor_details, name='visitor_details'),
     path('booking-success/', views.booking_success, name='booking_success'),
 ]
