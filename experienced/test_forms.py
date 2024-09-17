7421a9064f175f0209f15e60e55f47cbbb777ddd
Jxkeorton <jakeorton99@gmail.com>
2024-09-17 20:40:24 +0100

    Add experienced booking form test

---
+++ experienced/test_forms.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+from .forms import BookingForm
+
+class TestBookingForm(TestCase):
+    
+    def test_form_is_valid(self):
+        """ Test for all fields"""
+        form = BookingForm({
+            'jump_type': 'SOLO'
+        })
+        self.assertTrue(form.is_valid(), msg="Form is not valid")
+        
+    def test_form_wrong_jump_type(self):
+        """ Test for all fields"""
+        form = BookingForm({
+            'jump_type': 'JAZZ'
+        })
+        self.assertFalse(form.is_valid(), msg="Wrong jump type entered but Form is valid")
