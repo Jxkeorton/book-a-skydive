d908602d5c2c2840dce42155dcac9492835e0d67
Jxkeorton <jakeorton99@gmail.com>
2024-09-17 20:31:20 +0100

    Add edit_booking test

--- experienced/test_views.py
+++ experienced/test_views.py
@@ -25,17 +25,9 @@ class TestExperiencedViews(TestCase):
             departure=timezone.now() + timezone.timedelta(days=1),  # Schedule jump for tomorrow
             available_slots=self.plane.capacity
         )
-
-        # Create a JumpBooking
-        self.booking = JumpBooking(
-            user=self.user,
-            plane_departure=self.jump_slot,
-            jump_type="SOLO"
-        )
         
         self.plane.save()
         self.jump_slot.save()
-        self.booking.save()
         
     def test_render_plane_detail_with_booking_form(self):
         """
@@ -48,3 +40,53 @@ class TestExperiencedViews(TestCase):
         self.assertIn(b"Cessna", response.content)
         self.assertIsInstance(
             response.context['form'], BookingForm)
+        
+    def test_successful_booking_submission(self):
+        """Test for booking a specific plane departure"""
+        self.client.login(
+            username="myUsername", password="myPassword")
+        booking_data = {
+            'jump_type': 'SOLO'
+        }
+        
+        # Pass follow=True to follow the redirect after POST request
+        response = self.client.post(reverse(
+            'plane_detail', args=[self.jump_slot.slug]), booking_data, follow=True)
+        
+        # After following the redirect, the status code should be 200
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(
+            b'myUsername',
+            response.content
+        )
+        
+    def test_edit_booking(self):
+        """
+        Test editing an existing booking.
+        """
+        self.client.login(username="myUsername", password="myPassword")
+        
+        booking = JumpBooking(
+            user=self.user,
+            plane_departure=self.jump_slot,
+            jump_type="SOLO"
+        )
+        booking.save()
+        
+        self.assertEqual(JumpBooking.objects.filter(user=self.user).count(), 1)
+        updated_booking_data = {
+            'jump_type': 'WINGSUIT'
+        }
+        
+        response = self.client.post(
+            reverse('edit_booking', args=[booking.id]),
+            updated_booking_data,
+            follow=True
+        )
+
+        self.assertEqual(response.status_code, 200)
+        
+        booking.refresh_from_db()
+        self.assertEqual(booking.jump_type, 'WINGSUIT')
+        
+        self.assertIn(b'Booking updated successfully.', response.content)
